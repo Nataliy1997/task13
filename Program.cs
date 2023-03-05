cc94ed44f06936c2d5613d6e0e78a1bc8398c8b8
Natali <natalagurban7@gmail.com>
2023-03-05 16:39:10 +0300

    Решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Clear();
+Console.WriteLine("Введи любое число: ");
+int num=int.Parse(Console.ReadLine()!);
+int num1=num%100;
+int num2=num/10;
+int num3=num/100;
+if(num>=100)
+{
+    Console.Write($"Число {num}->{num3}");
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
